8f6580b2ff292849e226ed7e75461144a8ba0faa
Jacob-Parcell <jakeparcell14@gmail.com>
2026-07-25 17:49:41 -0700

    fixed navbar and removed unnecessary code

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,76 +9,93 @@ import {
 } from "@/components/ui/navigation-menu"
 
 import Link from "next/link"
+import { SectionKey } from "./spa-app"
 
-export function NavBar() {
+type NavBarProps = {
+  activeSection: SectionKey
+  setActiveSection?: (s: SectionKey) => void
+}
+
+export function NavBar({ activeSection, setActiveSection }: NavBarProps) {
   return (
     <div className="flex left">
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/">Home</Link>}
+              render={<p>Home</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("home")}
             />
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/schedule">Game Schedule</Link>}
+              render={<p>Schedule</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("schedule")}
             />
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/standings">Season Standings and Team Records</Link>}
+              render={<p>Season Standings and Team Records</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("standings")}
             />
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/results">Game Results</Link>}
+              render={<p>Game Results</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("results")}
             />
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/fieldinfo">Field Information</Link>}
+              render={<p>Field Information</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("fieldinfo")}
             />
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/forms">Forms</Link>}
+              render={<p>Forms</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("forms")}
             />
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/rules">Rules of the Game</Link>}
+              render={<p>Rules of the Game</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("rules")}
             />
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/leaguehistory">League History</Link>}
+              render={<p>League History</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("leaguehistory")}
             />
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/teamregistration">Team Registration</Link>}
+              render={<p>Team Registration</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("teamregistration")}
             />
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/lostandfound">Lost and Found</Link>}
+              render={<p>Lost and Found</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("lostandfound")}
             />
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink
-              render={<Link href="/contact">Contact Us</Link>}
+              render={<p>Contact Us</p>}
               className={navigationMenuTriggerStyle()}
+              onClick={() => setActiveSection?.("contact")}
             />
           </NavigationMenuItem>
         </NavigationMenuList>
